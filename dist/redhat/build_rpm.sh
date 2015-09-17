c4bfd15e8b7f45321258985c66e5059c1d4dcd40
Pekka Enberg <penberg@cloudius-systems.com>
2015-09-17 13:28:15 +0300

    dist/redhat: Fix generated tarball prefix
    Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

--- dist/redhat/build_rpm.sh
+++ dist/redhat/build_rpm.sh
@@ -13,7 +13,7 @@ VERSION=$(./SCYLLA-VERSION-GEN)
 SCYLLA_VERSION=$(cat build/SCYLLA-VERSION-FILE)
 SCYLLA_RELEASE=$(cat build/SCYLLA-RELEASE-FILE)
 mkdir -p $RPMBUILD/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
-git archive --format=tar --prefix=scylla-tools-$VERSION/ HEAD -o build/rpmbuild/SOURCES/scylla-tools-$VERSION.tar
+git archive --format=tar --prefix=scylla-tools-$SCYLLA_VERSION/ HEAD -o build/rpmbuild/SOURCES/scylla-tools-$VERSION.tar
 cp dist/redhat/scylla-tools.spec.in $RPMBUILD/SPECS/scylla-tools.spec
 sed -i -e "s/@@VERSION@@/$SCYLLA_VERSION/g" $RPMBUILD/SPECS/scylla-tools.spec
 sed -i -e "s/@@RELEASE@@/$SCYLLA_RELEASE/g" $RPMBUILD/SPECS/scylla-tools.spec
