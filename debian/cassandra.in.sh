d219e357d99f7e4cbab0b58361eaa0bfe290a50d
Shlomi Livne <shlomi@cloudius-systems.com>
2015-09-18 03:04:04 +0300

    Update debian/cassandra.in.sh for scylla used in rpm install
    Signed-off-by: Shlomi Livne <shlomi@cloudius-systems.com>
    Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

--- debian/cassandra.in.sh
+++ debian/cassandra.in.sh
@@ -1,6 +1,28 @@
+# Settings for Scylla
+SCYLLA_HOME=/var/lib/scylla
+SCYLLA_CONF="$SCYLLA_HOME/conf"
 
-# The directory where Cassandra's configs live (required)
-CASSANDRA_CONF=/etc/cassandra
+# Scylla adaption. Some one will still have to find us SCYLLA_HOME
+# or place us there.
+if [ -f "$SCYLLA_CONF/scylla.yaml" ]; then
+    if [ -f "$SCYLLA_CONF/cassandra.yaml" ]; then
+	CASSANDRA_CONF=$SCYLLA_CONF
+    else
+	# Create a temp config dir for just this execution
+	TMPCONF=`mktemp -d`
+	trap "rm -rf $TMPCONF" EXIT
+	cp -a "$SCYLLA_CONF"/* "$TMPCONF"
+	# Filter out scylla specific options that make
+	# cassandra options parser go boom.
+	# Also add attributes not present in scylla.yaml
+	# but required by cassandra.
+	`dirname $0`/filter_cassandra_attributes.py \
+		    "$TMPCONF/scylla.yaml" \
+		    > "$TMPCONF/cassandra.yaml"
+	CASSANDRA_CONF=$TMPCONF
+    fi
+fi
+
 
 CASSANDRA_HOME=/usr/share/cassandra
 
@@ -20,7 +42,6 @@ for jar in /usr/share/cassandra/*.jar; do
 done
 
 CLASSPATH="$CLASSPATH:$EXTRA_CLASSPATH"
-
 
 # set JVM javaagent opts to avoid warnings/errors
 if [ "$JVM_VENDOR" != "OpenJDK" -o "$JVM_VERSION" \> "1.6.0" ] \
