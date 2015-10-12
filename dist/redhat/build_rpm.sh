a5629903c2985509fa24fa66db6d9fe921211591
Shlomi Livne <shlomi@cloudius-systems.com>
2015-10-12 23:42:30 +0300

    dist: update rpm build to support centos as well
    Signed-off-by: Shlomi Livne <shlomi@cloudius-systems.com>
    Signed-off-by: Pekka Enberg <penberg@scylladb.com>

--- dist/redhat/build_rpm.sh
+++ dist/redhat/build_rpm.sh
@@ -1,14 +1,21 @@
 #!/bin/sh -e
 
-RPMBUILD=build/rpmbuild
+RPMBUILD=`pwd`/build/rpmbuild
 
 if [ ! -e dist/redhat/build_rpm.sh ]; then
-    echo "run build_rpm.sh in top of scylla dir"
+    echo "run build_rpm.sh in top of scylla-tools-java dir"
     exit 1
 fi
-if [ ! -f /usr/bin/git ] || [ ! -f /usr/bin/mock ] || [ ! -f /usr/bin/rpmbuild ]; then
-    sudo yum install -y git mock rpm-build
+
+sudo yum install -y rpm-build git
+
+OS=`awk '{print $1}' /etc/redhat-release`
+if [ "$OS" = "Fedora" ] && [ ! -f /usr/bin/mock ]; then
+    sudo yum -y install mock
+elif [ "$OS" = "CentOS" ] && [ ! -f /usr/bin/yum-builddep ]; then
+    sudo yum -y install yum-utils
 fi
+
 VERSION=$(./SCYLLA-VERSION-GEN)
 SCYLLA_VERSION=$(cat build/SCYLLA-VERSION-FILE)
 SCYLLA_RELEASE=$(cat build/SCYLLA-RELEASE-FILE)
@@ -17,5 +24,11 @@ git archive --format=tar --prefix=scylla-tools-$SCYLLA_VERSION/ HEAD -o build/rp
 cp dist/redhat/scylla-tools.spec.in $RPMBUILD/SPECS/scylla-tools.spec
 sed -i -e "s/@@VERSION@@/$SCYLLA_VERSION/g" $RPMBUILD/SPECS/scylla-tools.spec
 sed -i -e "s/@@RELEASE@@/$SCYLLA_RELEASE/g" $RPMBUILD/SPECS/scylla-tools.spec
-rpmbuild -bs --define "_topdir $RPMBUILD" $RPMBUILD/SPECS/scylla-tools.spec
-/usr/bin/mock rebuild --resultdir=`pwd`/build/rpms $RPMBUILD/SRPMS/scylla-tools-$VERSION*.src.rpm
+
+if [ "$OS" = "Fedora" ]; then
+    rpmbuild -bs --define "_topdir $RPMBUILD" $RPMBUILD/SPECS/scylla-tools.spec
+    /usr/bin/mock rebuild --resultdir=`pwd`/build/rpms $RPMBUILD/SRPMS/scylla-tools-$VERSION*.src.rpm
+else
+    sudo yum-builddep -y  $RPMBUILD/SPECS/scylla-tools.spec
+    rpmbuild -ba --define "_topdir $RPMBUILD" $RPMBUILD/SPECS/scylla-tools.spec
+fi
