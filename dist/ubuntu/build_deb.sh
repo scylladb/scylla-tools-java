07c222d62e88ac036acce6ac4090848659ed8c76
Takuya ASADA <syuu@scylladb.com>
2015-10-28 19:58:43 +0900

    dist: add devscripts to install debuild before start building package
    Signed-off-by: Takuya ASADA <syuu@scylladb.com>
    Signed-off-by: Pekka Enberg <penberg@scylladb.com>

--- dist/ubuntu/build_deb.sh
+++ dist/ubuntu/build_deb.sh
@@ -5,5 +5,5 @@ if [ ! -e dist/ubuntu/build_deb.sh ]; then
     exit 1
 fi
 
-sudo apt-get -y install debhelper openjdk-7-jdk ant ant-optional python-support dpatch bash-completion
+sudo apt-get -y install debhelper openjdk-7-jdk ant ant-optional python-support dpatch bash-completion devscripts
 debuild -r fakeroot --no-tgz-check -us -uc
