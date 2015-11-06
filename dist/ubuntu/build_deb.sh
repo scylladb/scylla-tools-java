b33e1a91280de8133270a3fa390d552d2dd15a25
Takuya ASADA <syuu@scylladb.com>
2015-11-07 05:22:37 +0900

    dist: support ./SCYLLA-VERSION-GEN on ubuntu package
    Signed-off-by: Takuya ASADA <syuu@scylladb.com>

--- dist/ubuntu/build_deb.sh
+++ dist/ubuntu/build_deb.sh
@@ -6,4 +6,15 @@ if [ ! -e dist/ubuntu/build_deb.sh ]; then
 fi
 
 sudo apt-get -y install debhelper openjdk-7-jdk ant ant-optional python-support dpatch bash-completion devscripts
+
+VERSION=$(./SCYLLA-VERSION-GEN)
+SCYLLA_VERSION=$(cat build/SCYLLA-VERSION-FILE)
+SCYLLA_RELEASE=$(cat build/SCYLLA-RELEASE-FILE)
+if [ "$SCYLLA_VERSION" = "development" ]; then
+	SCYLLA_VERSION=0development
+fi
+cp dist/ubuntu/changelog.in debian/changelog
+sed -i -e "s/@@VERSION@@/$SCYLLA_VERSION/g" debian/changelog
+sed -i -e "s/@@RELEASE@@/$SCYLLA_RELEASE/g" debian/changelog
+
 debuild -r fakeroot --no-tgz-check -us -uc
