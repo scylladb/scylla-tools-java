0f6d40ff364de8fa2f8a94b1351b6e1df307dcc1
Calle Wilund <calle@scylladb.com>
2016-08-08 13:13:21 +0000

    sstableloader: Add rate limiter
    Uses estimate of actual java driver CQL payload to adjust data rate.
    
    Message-Id: <1470662001-28047-1-git-send-email-calle@scylladb.com>

--- src/java/com/scylladb/tools/BulkLoader.java
+++ src/java/com/scylladb/tools/BulkLoader.java
@@ -111,6 +111,7 @@ import com.datastax.driver.core.TableMetadata;
 import com.datastax.driver.core.TokenRange;
 import com.datastax.driver.core.TupleType;
 import com.datastax.driver.core.UserType;
+import com.google.common.util.concurrent.RateLimiter;
 
 public class BulkLoader {
     public static class CmdLineOptions extends Options {
@@ -164,6 +165,9 @@ public class BulkLoader {
 
         private Object tokenKey;
 
+        private RateLimiter rateLimiter;
+        private int bytes;
+
         public CQLClient(LoaderOptions options, String keyspace)
                 throws NoSuchAlgorithmException, FileNotFoundException,
                 IOException, KeyStoreException, CertificateException,
@@ -213,6 +217,11 @@ public class BulkLoader {
             Schema.instance.load(ksMetaData);
             loadUserTypes(keyspaceMetadata.getUserTypes(), ksMetaData);
             partitioner = FBUtilities.newPartitioner(metadata.getPartitioner());
+
+            if (options.throttle != 0) {
+		// I seriously had no idea Mb was defined as 10^6, not 2^20. Go figure.
+                rateLimiter = RateLimiter.create(options.throttle * 1000 * 1000 / 8);
+            }
         }
 
         // Load user defined types. Since loading a UDT entails validation
@@ -292,12 +301,18 @@ public class BulkLoader {
             		for (Statement s : batchStatement.getStatements()) {
                         System.out.print("  ");
                         System.out.println(s);
-            		}
-            		System.out.println("END");            		
-            	}
-            	if (!simulate) {
-            		session.execute(batchStatement);
-            	}
+                    }
+                    System.out.println("END");
+                }
+                if (!simulate) {
+                    session.execute(batchStatement);
+                    if (rateLimiter != null) {
+                        // Acquire after execute, since bytes used are
+                        // calculated there.
+                        rateLimiter.acquire(bytes);
+                        bytes = 0;
+                    }
+                }
             }
             batchStatement = new BatchStatement();
         }
@@ -365,8 +380,26 @@ public class BulkLoader {
                 }
                 System.out.println();
             }
-            
-            SimpleStatement s = new SimpleStatement(what, objects.toArray());
+
+            SimpleStatement s = new SimpleStatement(what, objects.toArray()) {
+                @Override
+                public ByteBuffer[] getValues(ProtocolVersion protocolVersion) {
+                    ByteBuffer[] values = super.getValues(protocolVersion);
+                    if (rateLimiter != null) {
+                        // Try to guesstimate the bytes payload of the query
+                        // and add to bytes consumed by this batch.
+                        bytes += getQueryString().length();
+                        if (values != null) {
+                            for (ByteBuffer buf : values) {
+                                if (buf != null) {
+                                    bytes += buf.remaining();
+                                }
+                            }
+                        }
+                    }
+                    return values;
+                }
+            };
             s.setDefaultTimestamp(timestamp);
             s.setKeyspace(getKeyspace());
             s.setRoutingKey(key.getKey());
