01e085f4461b7e7e18eff43b22441ac7bf63cb98
Takuya ASADA <syuu@scylladb.com>
2016-12-29 18:35:26 +0900

    dist/ubuntu: check existance of build tools
    It normally won't be problem because scylla-tools-java usually build after scylla building.
    However some tools we required for build_deb.sh doesn't installed on minimal installation, so we should check and install it.
    
    Signed-off-by: Takuya ASADA <syuu@scylladb.com>
    Message-Id: <1483004126-2901-1-git-send-email-syuu@scylladb.com>

--- dist/ubuntu/build_deb.sh
+++ dist/ubuntu/build_deb.sh
@@ -9,6 +9,19 @@ if [ -e debian ] || [ -e build/release ]; then
     rm -rf debian build conf/hotspot_compiler
     mkdir build
 fi
+sudo apt-get -y update
+if [ ! -f /usr/bin/git ]; then
+    sudo apt-get -y install git
+fi
+if [ ! -f /usr/bin/mk-build-deps ]; then
+    sudo apt-get -y install devscripts
+fi
+if [ ! -f /usr/bin/equivs-build ]; then
+    sudo apt-get -y install equivs
+fi
+if [ ! -f /usr/bin/lsb_release ]; then
+    sudo apt-get -y install lsb-release
+fi
 
 DISTRIBUTION=`lsb_release -i|awk '{print $3}'`
 RELEASE=`lsb_release -r|awk '{print $2}'`
