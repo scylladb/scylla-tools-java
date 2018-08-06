39f6cce216784743ec24cc78ba4eaa30250e3e88
Piotr Jastrzebski <piotr@scylladb.com>
2018-08-06 22:36:08 +0200

    Add close and copy to Client
    Signed-off-by: Piotr Jastrzebski <piotr@scylladb.com>

--- src/java/com/scylladb/tools/Client.java
+++ src/java/com/scylladb/tools/Client.java
@@ -38,6 +38,10 @@ interface Client {
 
     void finish();
 
+    void close();
+
+    Client copy();
+
     CFMetaData getCFMetaData(String keyspace, String cfName);
 
     Map<InetAddress, Collection<Range<Token>>> getEndpointRanges();
