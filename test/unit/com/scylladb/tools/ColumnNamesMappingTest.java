f5c965cffbeaa4e08b4b616f6c63afd44d0b8531
Piotr Jastrzebski <piotr@scylladb.com>
2018-08-10 16:13:56 +0200

    Add unit test for ColumnNamesMapping
    Signed-off-by: Piotr Jastrzebski <piotr@scylladb.com>

---
+++ test/unit/com/scylladb/tools/ColumnNamesMappingTest.java
@@ -0,0 +1,95 @@
+package com.scylladb.tools;
+
+import java.io.File;
+import java.net.InetAddress;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.cassandra.config.CFMetaData;
+import org.apache.cassandra.db.DecoratedKey;
+import org.apache.cassandra.db.marshal.Int32Type;
+import org.apache.cassandra.dht.IPartitioner;
+import org.apache.cassandra.dht.Murmur3Partitioner;
+import org.apache.cassandra.dht.Range;
+import org.apache.cassandra.dht.Token;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ColumnNamesMappingTest {
+
+    private static final int THREADS_COUNT = 1;
+
+    private static class MockClient implements Client {
+
+        private final Set<String> statements = new HashSet<>();
+
+        @Override
+        public void close() {
+        }
+
+        @Override
+        public Client copy() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public CFMetaData getCFMetaData(String keyspace, String cfName) {
+            return CFMetaData.Builder.create("loader_test", "loader_test_table")
+                    .withPartitioner(new Murmur3Partitioner())
+                    .addPartitionKey("new_pk1", Int32Type.instance)
+                    .addPartitionKey("new_pk2", Int32Type.instance)
+                    .addClusteringColumn("new_ck1", Int32Type.instance)
+                    .addClusteringColumn("new_ck2", Int32Type.instance)
+                    .addRegularColumn("new_val1", Int32Type.instance)
+                    .addRegularColumn("new_val2", Int32Type.instance)
+                    .build();
+        }
+
+        @Override
+        public Map<InetAddress, Collection<Range<Token>>> getEndpointRanges() {
+            return null;
+        }
+
+        @Override
+        public IPartitioner getPartitioner() {
+            return new Murmur3Partitioner();
+        }
+
+        @Override
+        public void processStatment(DecoratedKey key, long timestamp, String what, List<Object> objects) {
+            statements.add(what);
+        }
+
+        public void assertStatements() {
+            Assert.assertTrue(statements.contains("INSERT INTO loader_test.loader_test_table (new_val1,new_pk1,new_pk2,new_ck1,new_ck2) values (?,?,?,?,?) USING TIMESTAMP ?;"));
+            Assert.assertTrue(statements.contains("UPDATE loader_test.loader_test_table USING TIMESTAMP ? SET new_val2 = ? WHERE new_pk1 = ? AND new_pk2 = ? AND new_ck1 = ? AND new_ck2 = ?;"));
+            Assert.assertTrue(statements.contains("DELETE FROM loader_test.loader_test_table USING TIMESTAMP ? WHERE new_pk1 = ? AND new_pk2 = ?;"));
+        }
+    }
+
+    @Test
+    public void testColumnNamesMapping() throws Exception
+    {
+        System.setProperty("cassandra.config", "file:///" + new File("test/conf/cassandra.yaml").getAbsolutePath());
+        final File dir = new File("test/data/scylla/column_names_mapping");
+        final String keyspace = "loader_test";
+
+        final Map<String, String> mapping = new HashMap<>();
+        mapping.put("pk1", "new_pk1");
+        mapping.put("pk2", "new_pk2");
+        mapping.put("ck1", "new_ck1");
+        mapping.put("ck2", "new_ck2");
+        mapping.put("val1", "new_val1");
+        mapping.put("val2", "new_val2");
+
+        MockClient client = new MockClient();
+        SSTableToCQL ssTableToCQL = new SSTableToCQL(keyspace, client, new ColumnNamesMapping(mapping), THREADS_COUNT);
+        ssTableToCQL.stream(dir);
+        client.assertStatements();
+    }
+
+}
