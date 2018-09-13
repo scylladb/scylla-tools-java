b073229b0f501d60df4c3bd19a6f2affcafe35ca
Calle Wilund <calle@scylladb.com>
2018-09-13 07:39:05 +0000

    sstableloader: Add progress feedback to user
    Fixes #63
    
    Adds stats collection, and (for now partial) printing of said stats at
    regular intervals to provide feedback on where the process is.
    
    To do this in any sane (not adding more psuedo-abstract interfaces) way,
    the main processing logic is fully moved into the bulkloader type, where
    it really belongs, and sstabletocql is reduced to exactly that;
    Translating an sstable (range) to CQL. This means that the actual tool
    type can fully control concurrency and ownerships of objects + we can
    remove some false abstractions from the interface types while still
    maintain unit testability.
    
    Reviewed-by: Piotr Jastrzebski <piotr@scylladb.com>
    Message-Id: <20180913073905.5095-1-calle@scylladb.com>

--- src/java/com/scylladb/tools/Client.java
+++ src/java/com/scylladb/tools/Client.java
@@ -23,27 +23,13 @@
 
 package com.scylladb.tools;
 
-import java.net.InetAddress;
-import java.util.Collection;
 import java.util.Map;
 
 import org.apache.cassandra.config.CFMetaData;
 import org.apache.cassandra.db.DecoratedKey;
-import org.apache.cassandra.dht.IPartitioner;
-import org.apache.cassandra.dht.Range;
-import org.apache.cassandra.dht.Token;
 
 interface Client {
-
-    void close();
-
-    Client copy();
-
     CFMetaData getCFMetaData(String keyspace, String cfName);
-
-    Map<InetAddress, Collection<Range<Token>>> getEndpointRanges();
-
-    IPartitioner getPartitioner();
 
     void processStatment(DecoratedKey key, long timestamp, String what, Map<String, Object> objects);
 }
