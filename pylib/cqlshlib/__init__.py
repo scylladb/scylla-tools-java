1592aa20fb41fed607ac48fd6ac1038f4bb6c665
Calle Wilund <calle@scylladb.com>
2019-03-04 12:57:31 +0000

    cqlsh: Add extension handler for "scylla_encryption_options"
    Refs #731
    
    Scylla encryption options are not stored with "normal" options.
    Because these are 100% intertwined with drivers, and adding
    stuff there breaks things.
    
    Instead they are extensions. The python driver allows adding
    extension handlers to print info about these.
    
    This adds a handler, and while the end result will not be
    the actual cql statement to recreate the table, it at least
    will provide feedback to the user.
    
    v2:
    * format comments
    * indentation

--- pylib/cqlshlib/__init__.py
+++ pylib/cqlshlib/__init__.py
@@ -13,3 +13,45 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+from cassandra.metadata import RegisteredTableExtension
+import io
+import struct
+
+# Add a handler for scylla_encryption_options extension to at least print 
+# the info when doing "desc <table>".
+#
+# The end result will not be cut-and-paste usable; we'd need to modify the
+# driver for this. But it is something. 
+class Encr(RegisteredTableExtension):
+    name = 'scylla_encryption_options'
+
+    @classmethod
+    def after_table_cql(cls, table_meta, ext_key, ext_blob):
+        # For scylla_encryption_options, the blob is actually 
+        # a serialized unorderd_map<string, string>. 
+        bytes = io.BytesIO(ext_blob)
+
+        def read_string():
+            # strings are little endian 32-bit len + bytes
+            utf_length = struct.unpack('<I', bytes.read(4))[0]
+            return bytes.read(utf_length)
+
+        def read_pair():
+            # each map::value_type is written as <string><string>
+            key = read_string()
+            val = read_string()
+            return key, val
+
+        def read_map():
+            # num elements
+            len = struct.unpack('<I', bytes.read(4))[0]
+            res = {}
+            # x value_type pairs
+            for x in range(0, len):
+                p = read_pair()
+                res[p[0]] = p[1]
+            return res
+
+        return "%s = %s" % (ext_key, read_map())
+
