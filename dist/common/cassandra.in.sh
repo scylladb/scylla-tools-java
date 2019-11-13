c9cc08c5949ce7eef5fde7be39a79095d99ced02
Avi Kivity <avi@scylladb.com>
2019-11-13 14:13:22 +0200

    Revert "Merge "nonroot installer" from Takuya"
    This reverts commit a7353939a02295189081a1f7297d591343267d9e, reversing
    changes made to 22aaca7c60533da4f77388044b2274db42de18ad. nodetool
    and cassandra-stress stopped working after this commit.
    
    Ref scylladb/scylla#5265.

--- dist/common/cassandra.in.sh
+++ dist/common/cassandra.in.sh
@@ -36,7 +36,7 @@ if [ -f "$SCYLLA_CONF/scylla.yaml" ]; then
 fi
 
 
-CASSANDRA_HOME=/opt/scylladb/share/cassandra
+CASSANDRA_HOME=/usr/share/scylla/cassandra
 
 # The java classpath (required)
 if [ -n "$CLASSPATH" ]; then
@@ -45,11 +45,11 @@ else
     CLASSPATH=$CASSANDRA_CONF
 fi
 
-for jar in /opt/scylladb/share/cassandra/lib/*.jar; do
+for jar in /usr/share/scylla/cassandra/lib/*.jar; do
     CLASSPATH=$CLASSPATH:$jar
 done
 
-for jar in /opt/scylladb/share/cassandra/*.jar; do
+for jar in /usr/share/scylla/cassandra/*.jar; do
     CLASSPATH=$CLASSPATH:$jar
 done
 
