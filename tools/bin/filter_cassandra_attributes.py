9d79dd384b9abb7756bc11bd8a9b96a08aac814f
Alexys Jacob <ultrabug@gentoo.org>
2019-11-14 21:31:29 +0100

    filter_cassandra_attributes.py: migrate from unsafe yaml.load()
    Read https://msg.pyyaml.org/load for full details (CVE-2017-18342)
    
    Fix it by use new safe interface - yaml.safe_load()

--- tools/bin/filter_cassandra_attributes.py
+++ tools/bin/filter_cassandra_attributes.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python2
 
-import sys;
-from yaml import load, dump, load_all
+import sys
+from yaml import dump, safe_load
 from cassandra_attributes import *
 
 def main():
     attributes = dict()
     for i in range(1, len(sys.argv)):
-        attributes.update(load(open(sys.argv[i], 'r')))
-    print dump(dict(filter(lambda (a, b): a in cassandra_attributes, attributes.items())))                   
+        attributes.update(safe_load(open(sys.argv[i], 'r')))
+    print dump(dict(filter(lambda (a, b): a in cassandra_attributes, attributes.items())))
 
 if __name__ == "__main__":
     main()
