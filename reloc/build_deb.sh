40a65fbd63a1868fef40f8937d1a083a198fdc96
Pekka Enberg <penberg@scylladb.com>
2020-07-17 11:57:16 +0300

    reloc: Add "--builddir" option to build_{rpm,deb}.sh
    We need the ability to control build directory in scylla.git build
    system. Let's add support for the "--builddir" option like in other
    variants of the same scripts.
    
    Message-Id: <20200717085716.701154-1-penberg@scylladb.com>

--- reloc/build_deb.sh
+++ reloc/build_deb.sh
@@ -4,16 +4,22 @@
 print_usage() {
     echo "build_deb.sh --reloc-pkg build/scylla-tools-package.tar.gz"
     echo "  --reloc-pkg specify relocatable package path"
+    echo "  --builddir specify Debian package build path"
     exit 1
 }
 
 RELOC_PKG=$(readlink -f build/scylla-tools-package.tar.gz)
+BUILDDIR=build/debian
 OPTS=""
 while [ $# -gt 0 ]; do
     case "$1" in
         "--reloc-pkg")
             OPTS="$OPTS $1 $(readlink -f $2)"
             RELOC_PKG=$2
+            shift 2
+            ;;
+        "--builddir")
+            BUILDDIR="$2"
             shift 2
             ;;
         *)
@@ -25,8 +31,8 @@ done
 if [[ ! $OPTS =~ --reloc-pkg ]]; then
     OPTS="$OPTS --reloc-pkg $RELOC_PKG"
 fi
-rm -rf build/debian
-mkdir -p build/debian/scylla-package
-tar -C build/debian/scylla-package -xpf $RELOC_PKG
-cd build/debian/scylla-package
+rm -rf "$BUILDDIR "
+mkdir -p "$BUILDDIR"/scylla-package
+tar -C "$BUILDDIR"/scylla-package -xpf $RELOC_PKG
+cd "$BUILDDIR"/scylla-package
 exec bash -x -e ./scylla-tools/dist/debian/build_deb.sh $OPTS
--- reloc/build_rpm.sh
+++ reloc/build_rpm.sh
@@ -4,15 +4,21 @@
 print_usage() {
     echo "build_rpm.sh --reloc-pkg build/scylla-tools-package.tar.gz"
     echo "  --reloc-pkg specify relocatable package path"
+    echo "  --builddir specify rpmbuild directory"
     exit 1
 }
 RELOC_PKG=$(readlink -f build/scylla-tools-package.tar.gz)
+BUILDDIR=build/redhat
 OPTS=""
 while [ $# -gt 0 ]; do
     case "$1" in
         "--reloc-pkg")
             OPTS="$OPTS $1 $(readlink -f $2)"
             RELOC_PKG=$2
+            shift 2
+            ;;
+        "--builddir")
+            BUILDDIR="$2"
             shift 2
             ;;
         *)
@@ -24,7 +30,7 @@ done
 if [[ ! $OPTS =~ --reloc-pkg ]]; then
     OPTS="$OPTS --reloc-pkg $RELOC_PKG"
 fi
-mkdir -p build/redhat/
-tar -C build/redhat/ -xpf $RELOC_PKG scylla-tools/SCYLLA-RELEASE-FILE scylla-tools/SCYLLA-RELOCATABLE-FILE scylla-tools/SCYLLA-VERSION-FILE scylla-tools/SCYLLA-PRODUCT-FILE scylla-tools/dist/redhat
-cd build/redhat/scylla-tools
+mkdir -p "$BUILDDIR"
+tar -C "$BUILDDIR" -xpf $RELOC_PKG scylla-tools/SCYLLA-RELEASE-FILE scylla-tools/SCYLLA-RELOCATABLE-FILE scylla-tools/SCYLLA-VERSION-FILE scylla-tools/SCYLLA-PRODUCT-FILE scylla-tools/dist/redhat
+cd "$BUILDDIR"/scylla-tools
 exec ./dist/redhat/build_rpm.sh $OPTS
